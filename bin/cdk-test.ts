2b83aabfc5ffdb5ca2ba3ec70c2403d5b9a3a77c
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-07-23 16:19:10 +0900

    add account env

--- bin/cdk-test.ts
+++ bin/cdk-test.ts
@@ -3,6 +3,8 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { CdkTestStack } from "../lib/cdk-test-stack";
 
+require("dotenv").config();
+
 const app = new cdk.App();
 new CdkTestStack(app, "CdkTestStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
@@ -10,7 +12,10 @@ new CdkTestStack(app, "CdkTestStack", {
    * but a single synthesized template can be deployed anywhere. */
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
