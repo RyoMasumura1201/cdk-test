9ca26c320aeda58a0e3d6efb05ccaddc9bf01f3b
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-07-23 16:45:02 +0900

    add vpc and subnet

--- lib/cdk-test-stack.ts
+++ lib/cdk-test-stack.ts
@@ -1,16 +1,26 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Vpc, SubnetType } from "aws-cdk-lib/aws-ec2";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkTestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new Vpc(this, "CDKTest", {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "west-subnet",
+          subnetType: SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "east-subnet",
+          subnetType: SubnetType.PUBLIC,
+        },
+      ],
+    });
   }
 }
